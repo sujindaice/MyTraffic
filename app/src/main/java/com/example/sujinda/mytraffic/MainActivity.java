1ec6985dee59ddb4a43e8cf94e7aed2d2e14b4eb
jae <jzeed.blackboy@gmail.com>
2015-12-19 14:46:07 +0700

    ออกแบบ Textview เสร็จแล้ว

--- app/src/main/java/com/example/sujinda/mytraffic/MainActivity.java
+++ app/src/main/java/com/example/sujinda/mytraffic/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.sujinda.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+    }   //Main method
+
+    private void buttonController() {
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //  Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.phonton1);
+                buttonMediaPlayer.start();
+            }   //Event
+        });
     }
-}
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutButton = (Button) findViewById(R.id.button);
+    }
+}   //Main Class
